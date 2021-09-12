870492c5d539d9221e9edc454e43624136f6d4ab
Jakson Moreno <jak.moreno@gmail.com>
2021-09-12 13:30:53 -0500

    Realice el código para hacer los calculos de perimetro y area del cuadrado

--- figuras.js
+++ figuras.js
@@ -1,1 +1,37 @@
-console.log("Hello, World!")
+//Código del cuadrado
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
+
+const perimetroCuaadrado = ladoCuadrado * 4; 
+console.log("El perimetro del cuadrado es: " + perimetroCuaadrado + " cm");
+
+const areaCuaadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuaadrado + " cm^2");
+console.groupEnd();
+
+//Código del triangulo
+console.group("Triangulos");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+
+console.log(
+    "Los lados del cuadrado miden: "
+    + ladoTriangulo1
+    + " cm, "
+    + ladoTriangulo2
+    + " cm, "
+    + baseTriangulo
+    + " cm "
+)
+
+
+console.groupEnd();
+
+//Código del Circulo
+console.group("Circulos");
+
+
+console.groupEnd();
